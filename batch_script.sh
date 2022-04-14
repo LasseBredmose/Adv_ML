c10c113eb621d36f54ffe45b9d63e446557bd3e0
Lasse <s175395@student.dtu.dk>
2022-04-14 10:16:44 +0200

    New model trans is fixed

--- batch_script.sh
+++ batch_script.sh
@@ -1,13 +1,19 @@
 #!/bin/sh 
+# Load dependencies
+module load python3/3.9.6
+# module load matplotlib/3.4.2-numpy-1.21.1-python-3.9.6
+module load pandas/1.3.1-python-3.9.6
+module load cuda/11.5
+module load cudnn/v8.3.0.98-prod-cuda-11.5
 
-pip intall -r requirements.txt
+python3 -m pip install --user -r requirements.txt
 
 ### General options 
 ### -- specify queue -- 
 #BSUB -q gpuv100
 #BSUB -gpu "num=1"
 ### -- set the job Name -- 
-#BSUB -J 5_CNN_trans
+#BSUB -J 5_CNN_13_4
 ### -- ask for number of cores (default: 1) -- 
 #BSUB -n 1 
 ### -- specify that the cores must be on the same host -- 
@@ -26,15 +32,10 @@ pip intall -r requirements.txt
 #BSUB -N 
 ### -- Specify the output and error file. %J is the job-id -- 
 ### -- -o and -e mean append, -oo and -eo mean overwrite -- 
-#BSUB -o 5_CNN_trans.out 
-#BSUB -e 5_CNN_trans.err 
-# Load dependencies
-module load python3/3.9.6
-# module load matplotlib/3.4.2-numpy-1.21.1-python-3.9.6
-module load pandas/1.3.1-python-3.9.6
-module load cuda/11.5
-module load cudnn/v8.3.0.98-prod-cuda-11.5
+#BSUB -o 5_CNN_13_4.out 
+#BSUB -e 5_CNN_13_4.err 
+
 
 
 # here follow the commands you want to execute 
-python3 main.py train 0 1  #>> laplace_$hess.out #&> Test1.out
+python3 main.py train 0 0  #>> laplace_$hess.out #&> Test1.out
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -20,7 +20,7 @@ from torch.nn.functional import softmax
 warnings.filterwarnings("ignore")
 
 
-def train(small, transform):
+def train(small, transf):
     # Cuda Stuff
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -120,7 +120,7 @@ def train(small, transform):
             inputs, labels = Variable(get_variable(inputs)), Variable(
                 get_variable(labels)
             )
-            if transform == 1:
+            if transf == 1:
                 trans = ChooseTrans(ChosenTrans)
                 inputs = trans(inputs)
         
@@ -175,12 +175,12 @@ def train(small, transform):
     print("Finished !!")
 
     # Storing the model
-    date_time = datetime.now().strftime("%d/%m_%H")
+    date_time = datetime.now().strftime("%d_%m_%H")
     model.load_state_dict(best_model)
     # torch.save(model, f'./models/trained_model_epocs{num_epochs}_{date_time}.pt')
     torch.save(
         model.state_dict(),
-        f"./models/STATEtrained_model_epocs{num_epochs}_{date_time}_trans_{transform}.pt",
+        f"./models/STATEtrained_model_epocs{num_epochs}_{date_time}_trans_{transf}.pt",
     )
 
     model.eval()
@@ -208,4 +208,4 @@ def train(small, transform):
         range(num_epochs), train_loss_epoch, range(num_epochs), validation_loss_epoch
     )
     plt.legend(["Training data", "Validation data"])
-    plt.savefig(f"./reports/epocs{num_epochs}_{date_time}_trans_{transform}")
+    plt.savefig(f"./reports/epocs{num_epochs}_{date_time}_trans_{transf}")
